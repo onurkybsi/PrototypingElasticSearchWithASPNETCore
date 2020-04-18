933a2c1e00d64a829469ab25fe538d6482023a2c
onurkybsi <onurbpm@outlook.com>
2020-04-18 15:15:43 +0300

    Commit3

---
+++ ElasticSearchClient/wwwroot/js/dataFilter.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#myInput").keypress(function (e) {
+        $.ajax({
+            url: "http://localhost:5001/api/ElasticSearch/" + e.key,
+            method: "GET",
+            success: function (data) {
+                listData(data);
+            }
+        })
+    });
+});
+
+var listData = function (data) {
+    $("#myUL").append("<li>" +
+        data["firstName"] +
+        "</li>");
+};
